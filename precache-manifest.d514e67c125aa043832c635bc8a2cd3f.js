957c26705dde3d08422f0e72bdfa8d24f281718d
yiannis pipertzis <yiannn@gmail.com>
2024-05-30 20:27:40 +1000

    Updates

--- precache-manifest.d514e67c125aa043832c635bc8a2cd3f.js
+++ precache-manifest.d514e67c125aa043832c635bc8a2cd3f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e15d6ff32e65d6c13f9698059d4cd28d",
+    "revision": "51b758cc26c7182ffde17b6c5462c0c4",
     "url": "/teotibot/index.html"
   },
   {
-    "revision": "48969547d254e445d6d0",
+    "revision": "9ac1e85da9cfb659735a",
     "url": "/teotibot/static/css/2.ab540311.chunk.css"
   },
   {
-    "revision": "6d1c1bfa28ff6d80d444",
+    "revision": "07dcd936304e2ca16005",
     "url": "/teotibot/static/css/main.e081d23a.chunk.css"
   },
   {
-    "revision": "48969547d254e445d6d0",
-    "url": "/teotibot/static/js/2.c2511e0d.chunk.js"
+    "revision": "9ac1e85da9cfb659735a",
+    "url": "/teotibot/static/js/2.d5ea9ff8.chunk.js"
   },
   {
-    "revision": "6d1c1bfa28ff6d80d444",
-    "url": "/teotibot/static/js/main.194da8c3.chunk.js"
+    "revision": "07dcd936304e2ca16005",
+    "url": "/teotibot/static/js/main.31fef693.chunk.js"
   },
   {
     "revision": "5c70a438e702b569ea43",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teotibot/precache-manifest.b91b31680fc177107aae60036979d854.js"
+  "/teotibot/precache-manifest.d514e67c125aa043832c635bc8a2cd3f.js"
 );
 
 self.addEventListener('message', (event) => {
