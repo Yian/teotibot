8a75d8a21999090e574de9890c91634a0be77391
yiannis pipertzis <yiannn@gmail.com>
2024-05-30 22:32:50 +1000

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b91b31680fc177107aae60036979d854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15d6ff32e65d6c13f9698059d4cd28d",
+    "url": "/teotibot/index.html"
+  },
+  {
+    "revision": "48969547d254e445d6d0",
+    "url": "/teotibot/static/css/2.ab540311.chunk.css"
+  },
+  {
+    "revision": "6d1c1bfa28ff6d80d444",
+    "url": "/teotibot/static/css/main.e081d23a.chunk.css"
+  },
+  {
+    "revision": "48969547d254e445d6d0",
+    "url": "/teotibot/static/js/2.c2511e0d.chunk.js"
+  },
+  {
+    "revision": "6d1c1bfa28ff6d80d444",
+    "url": "/teotibot/static/js/main.194da8c3.chunk.js"
+  },
+  {
+    "revision": "5c70a438e702b569ea43",
+    "url": "/teotibot/static/js/runtime~main.97619256.js"
+  },
+  {
+    "revision": "234229e0381e3913d74381519936405c",
+    "url": "/teotibot/static/media/Mayan.234229e0.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b85040c74962e936a028c225c17026c3.js"
+  "/teotibot/precache-manifest.b91b31680fc177107aae60036979d854.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teotibot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
